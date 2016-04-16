2f41ded7121d0c291aa32971fcfeab709cb0436e
wadezyb <wadezyb@gmail.com>
2016-04-16 15:13:39 +0000

    initial ros workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/root/ros_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/root/ros_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/root/ros_ws/devel/lib:/root/ros_ws/devel/lib/arm-linux-gnueabihf:/opt/ros/jade/lib/arm-linux-gnueabihf:/opt/ros/jade/lib"
+export PATH="/root/ros_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/root/ros_ws/devel/lib/pkgconfig:/root/ros_ws/devel/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/jade/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/root/ros_ws/build"
+export PYTHONPATH="/root/ros_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/root/ros_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/root/ros_ws/src:$ROS_PACKAGE_PATH"
